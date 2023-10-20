a06df7f3977d7e8c491a7a4dad67b038a79298f4
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-10-20 12:28:09 +0300

    Create a Create Report Dto

---
+++ src/reports/dto/create-report.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  id: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2023)
+  year: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  kilometers: number;
+}
